6008d3244ab1bb9ce84858ee5c4e3e9188601851
wskim_inAcdm <devlunch4@gmail.com>
2021-01-06 20:58:44 +0900

    20210106 2100 python variable value graph finished.

--- HELLOPYTHON/day11/myGraph03plus.py
+++ HELLOPYTHON/day11/myGraph03plus.py
@@ -1,10 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 import pymysql
 
 
-# 함수화 
+# 함수화를 위한 클래스 설정
 class MyManager:
 
     def __init__(self):
@@ -27,12 +26,10 @@ class MyManager:
     
     def setlinexy(self):
         print("setline 진입")
-
         sql = "SELECT s_code, s_name, s_price, in_time FROM stock WHERE s_name = '삼성전자' ORDER BY in_time DESC;"
         self.cursor.execute(sql)
         rows = self.cursor.fetchall()
         return len(rows)
-    
     
     def getName(self):
         print("getName 진입")
@@ -40,6 +37,30 @@ class MyManager:
         self.cursor.execute(sql)
         namerows = self.cursor.fetchall()
         return namerows
+    
+    def getPricesPer(self, s_name):
+        # 초기값 가져오기
+        sql = "SELECT s_code, s_name, s_price, in_time FROM stock WHERE s_name = %s ORDER BY in_time"
+        self.cursor.execute(sql, (s_name,))
+        rows = self.cursor.fetchall()
+        self.fvalue = (str(rows[0][2]))
+        print("self.fvalue 시작가 :", self.fvalue)
+        # 최종값 설정
+        sql = "SELECT s_code, s_name, s_price, in_time FROM stock WHERE s_name = %s ORDER BY in_time"
+        self.cursor.execute(sql, (s_name,))
+        rows = self.cursor.fetchall()
+        prices = []
+
+        for row in rows:
+            # print("getPricesPer변화율:",row[1], "변화율 :", (row[2] - rows[0][2]) / rows[0][2] * 100, "시간 :", row[3])
+            zerobreak = rows[0][2]
+            pvalue = row[2]
+            if zerobreak == 0:
+                zerobreak = 1
+                pvalue = 1
+            prices.append((pvalue - zerobreak) / zerobreak * 100)
+        return prices
+
 
 # MyManager CALL
 mm = MyManager()
@@ -48,8 +69,7 @@ mm = MyManager()
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-
-
+# x, y 배열 설정
 lenonecode = int(mm.setlinexy())
 xarray = []
 yarray = []         
@@ -57,33 +77,23 @@ zarray = []
 for i in range(lenonecode):
             xarray.append(0)
             yarray.append(0 + i)
-            
-print("xarray",xarray, "yarray", yarray)
+print("xarray", xarray, "yarray", yarray)
 x = np.array(xarray)
 y = np.array(yarray)
 
-z1 = np.array(mm.getPrices('삼성전자'))
-z2 = np.array(mm.getPrices('LG'))
-z3 = np.array(mm.getPrices('SK'))
-print("z3 pirxe:",z3)
-
-print("mm.getName()", mm.getName())
-print("len mm.getName()", len(mm.getName()))
-
+# 이름 배열 설정
 namelist = mm.getName()
-zarray = []
 
+# xyz 설정 
+zarray = []
 xplus = 0
 for i in namelist:
-    zarray = mm.getPrices(i)
+    zarray = mm.getPricesPer(i)
     z = zarray
-    ax.plot(x+xplus, y, z)
-    xplus +=1
-print ("최종",z)
-# plot test data
-
-
-
+    xplus += 1
+    ax.plot(x + xplus, y, z)
+    
+print ("마지막 x 배열", x, "마지막 z 배열", z)
 
 # make labels
 ax.set_xlabel('X')
@@ -91,3 +101,4 @@ ax.set_ylabel('Y')
 ax.set_zlabel('Z')
 
 plt.show()
+print ("출력완료!!!")
